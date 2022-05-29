809496c55f9be481329ebf46d425c421d9e3c6ca
Tyler Lampel <lampel13@gmail.com>
2022-05-29 08:33:23 -0400

    commit tree 52a5580c641a407e34d0f8dd59512b20c827ccec

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // iterate from the beginning to middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to corresponding letter from end
+    const j = word.length - 1 - i;
+    // if any letters dont match return false
+    if (word[i] !== word[j]) return false;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  iterate from the beginning to the middle
+  check each character to the corresponding letter from the end
+  if any letters dont match, return false
+
+  return true
 */
 
 /*
-  Add written explanation of your solution here
+  make a function that returns true if a word is a palindrome. that means if the first letter
+  is the same as the last, the 2nd is the same as the 2nd to last, etc. until we reach the middle
+  return true
 */
 
 // You can run `node index.js` to view these console logs
